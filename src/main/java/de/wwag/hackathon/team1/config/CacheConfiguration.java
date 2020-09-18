60a1f4ec5fe21856774cc1108ad4fcf0deecdd63
Florian Herzog <herzog-florian@gmx.de>
2020-09-19 01:15:08 +0200

    remove spring cloud config and eureka from jhipster app

--- src/main/java/de/wwag/hackathon/team1/config/CacheConfiguration.java
+++ src/main/java/de/wwag/hackathon/team1/config/CacheConfiguration.java
@@ -16,9 +16,6 @@ import org.springframework.cache.interceptor.KeyGenerator;
 import org.springframework.beans.factory.annotation.Autowired;
 import io.github.jhipster.config.cache.PrefixedKeyGenerator;
 import org.springframework.cache.annotation.EnableCaching;
-import org.springframework.cloud.client.ServiceInstance;
-import org.springframework.cloud.client.discovery.DiscoveryClient;
-import org.springframework.cloud.client.serviceregistry.Registration;
 import org.springframework.context.annotation.*;
 
 @Configuration
