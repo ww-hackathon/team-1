f45580627c8f7b57bb49a53e328e7c50a6ed60ac
Mustert <christopher.mustert@gmx.de>
2020-09-24 14:39:37 +0200

    added service calls

--- src/main/webapp/app/entities/gruppe/gruppe.service.ts
+++ src/main/webapp/app/entities/gruppe/gruppe.service.ts
@@ -2,10 +2,13 @@ import { Injectable } from '@angular/core';
 import { HttpClient, HttpResponse } from '@angular/common/http';
 import { Observable } from 'rxjs';
 
+import { DATE_FORMAT } from 'app/shared/constants/input.constants';
 import { IGruppe } from '../../shared/model/gruppe.model';
 import { SERVER_API_URL } from '../../app.constants';
 import { createRequestOption } from '../../shared/util/request-util';
+import { Moment } from 'moment';
 
+type EntityResponseType = HttpResponse<IGruppe>;
 type EntityArrayResponseType = HttpResponse<IGruppe[]>;
 
 @Injectable({ providedIn: 'root' })
@@ -14,10 +17,17 @@ export class GruppenService {
 
   constructor(protected http: HttpClient) {}
 
-  //findByRoomIdAndDate
-
   query(req?: any): Observable<EntityArrayResponseType> {
     const options = createRequestOption(req);
     return this.http.get<IGruppe[]>(this.resourceUrl, { params: options, observe: 'response' });
+  }
+
+  findByRoomAndDate(roomId: number, date: moment.Moment): Observable<EntityResponseType> {
+    const formattedDate = this.formatDate(date);
+    return this.http.get<IGruppe>(`${this.resourceUrl}/${formattedDate}/raum/${roomId}`, { observe: 'response' });
+  }
+
+  private formatDate(moment?: Moment): string {
+    return moment && moment.isValid() ? moment.format(DATE_FORMAT) : '';
   }
 }
